ffc27422e6ded0c3986a420c201143f31bf47e05
GaHoWong <GaHoWong@outlook.com>
2020-07-19 23:04:09 +0800

    修改了pid

--- MIPS32CPU_GaHo/MIPS32CPU_GaHo.srcs/sources_1/new/id.v
+++ MIPS32CPU_GaHo/MIPS32CPU_GaHo.srcs/sources_1/new/id.v
@@ -3,34 +3,34 @@
 
 module id(
 
-	input wire										rst,
-	input wire[`InstAddrBus]			pc_i,
-	input wire[`InstBus]          inst_i,
+	input wire rst,
+	input wire[`InstAddrBus] pc_i,
+	input wire[`InstBus] inst_i,
 
   //处于执行阶段的指令的一些信息，用于解决load相关
-  input wire[`AluOpBus]					ex_aluop_i,
+  input wire[`AluOpBus] ex_aluop_i,
 
 	//处于执行阶段的指令要写入的目的寄存器信息
-	input wire										ex_wreg_i,
-	input wire[`RegBus]						ex_wdata_i,
-	input wire[`RegAddrBus]       ex_wd_i,
+	input wire ex_wreg_i,
+	input wire[`RegBus] ex_wdata_i,
+	input wire[`RegAddrBus] ex_wd_i,
 	
 	//处于访存阶段的指令要写入的目的寄存器信息
-	input wire										mem_wreg_i,
-	input wire[`RegBus]						mem_wdata_i,
-	input wire[`RegAddrBus]       mem_wd_i,
+	input wire mem_wreg_i,
+	input wire[`RegBus]	 mem_wdata_i,
+	input wire[`RegAddrBus] mem_wd_i,
 	
-	input wire[`RegBus]           reg1_data_i,
-	input wire[`RegBus]           reg2_data_i,
+	input wire[`RegBus] reg1_data_i,
+	input wire[`RegBus] reg2_data_i,
 
 	//如果上一条指令是转移指令，那么下一条指令在译码的时候is_in_delayslot为true
 	input wire                    is_in_delayslot_i,
 
 	//送到regfile的信息
-	output reg                    reg1_read_o,
-	output reg                    reg2_read_o,     
-	output reg[`RegAddrBus]       reg1_addr_o,
-	output reg[`RegAddrBus]       reg2_addr_o, 	      
+	output reg reg1_read_o,
+	output reg reg2_read_o,     
+	output reg[`RegAddrBus] reg1_addr_o,
+	output reg[`RegAddrBus] reg2_addr_o, 	      
 	
 	//送到执行阶段的信息
 	output reg[`AluOpBus]         aluop_o,
