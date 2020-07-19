e710d6f6ec8b6fce9dfdfa9a6223f6e807d583e5
GaHoWong <GaHoWong@outlook.com>
2020-07-19 10:38:08 +0800

    修改了执行阶段的ex.v

--- MIPS32CPU_GaHo/MIPS32CPU_GaHo.srcs/sources_1/new/ex.v
+++ MIPS32CPU_GaHo/MIPS32CPU_GaHo.srcs/sources_1/new/ex.v
@@ -21,6 +21,8 @@ module ex(
 );
 
 	reg[`RegBus] logicout;
+	reg[`RegBus] shiftres;
+		
 	always @ (*) begin
 		if(rst == `RstEnable) begin
 			logicout <= `ZeroWord;
@@ -29,8 +31,39 @@ module ex(
 				`EXE_OR_OP:			begin
 					logicout <= reg1_i | reg2_i;
 				end
+				`EXE_AND_OP:		begin
+					logicout <= reg1_i & reg2_i;
+				end
+				`EXE_NOR_OP:		begin
+					logicout <= ~(reg1_i |reg2_i);
+				end
+				`EXE_XOR_OP:		begin
+					logicout <= reg1_i ^ reg2_i;
+				end
 				default:				begin
 					logicout <= `ZeroWord;
+				end
+			endcase
+		end    //if
+	end      //always
+
+	always @ (*) begin
+		if(rst == `RstEnable) begin
+			shiftres <= `ZeroWord;
+		end else begin
+			case (aluop_i)
+				`EXE_SLL_OP:			begin
+					shiftres <= reg2_i << reg1_i[4:0] ;
+				end
+				`EXE_SRL_OP:		begin
+					shiftres <= reg2_i >> reg1_i[4:0];
+				end
+				`EXE_SRA_OP:		begin
+					shiftres <= ({32{reg2_i[31]}} << (6'd32-{1'b0, reg1_i[4:0]})) 
+												| reg2_i >> reg1_i[4:0];
+				end
+				default:				begin
+					shiftres <= `ZeroWord;
 				end
 			endcase
 		end    //if
@@ -44,6 +77,9 @@ module ex(
 	 	`EXE_RES_LOGIC:		begin
 	 		wdata_o <= logicout;
 	 	end
+	 	`EXE_RES_SHIFT:		begin
+	 		wdata_o <= shiftres;
+	 	end	 	
 	 	default:					begin
 	 		wdata_o <= `ZeroWord;
 	 	end
